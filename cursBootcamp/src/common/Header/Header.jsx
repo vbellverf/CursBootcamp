62aceaed5ea5803f25785cd87ea20d5c98f79185
vbellverf <victorbellverfornet@outlook.es>
2023-12-10 20:35:36 +0100

    a falta del search y info pelicula al pulsar sobre caratula

--- cursBootcamp/src/common/Header/Header.jsx
+++ cursBootcamp/src/common/Header/Header.jsx
@@ -10,8 +10,6 @@ export default function Header () {
       setActiveLink(index);
     }
 
-
-
     return (
         <nav className="HeaderDesign">
             <Link
--- cursBootcamp/src/pages/Popular/Popular.jsx
+++ cursBootcamp/src/pages/Popular/Popular.jsx
@@ -1,4 +1,3 @@
-import { json } from 'react-router-dom';
 import './Popular.css'
 import { useEffect, useState } from "react";
 
